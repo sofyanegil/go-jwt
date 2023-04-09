6ae6df627b34bb9b07c2a20973f833921d0b940f
sofyanegi <sofyanegil@gmail.com>
2023-04-09 14:20:58 +0700

    User and Product with jwt

---
+++ database/db.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"fmt"
+	"go-jwt/models"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	host     = "localhost"
+	user     = "postgres"
+	password = "root"
+	dbPort   = "5432"
+	dbName   = "db-go"
+	db       *gorm.DB
+	err      error
+)
+
+func StartDB() {
+	config := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbName, dbPort)
+	dsn := config
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("error connecting to database:", err)
+	}
+
+	fmt.Println("Success connected to database")
+	db.Debug().AutoMigrate(models.User{}, models.Product{})
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
